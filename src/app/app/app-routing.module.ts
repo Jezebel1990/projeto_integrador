9546ec1e4a9012e7b1f982633cb398b8ad7ac475
Jezebel1990 <75287031+Jezebel1990@users.noreply.github.com>
2021-09-26 17:55:57 -0300

    Add files via upload

---
+++ src/app/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+
+import { PaginaInicialModule } from './pagina-inicial/pagina-inicial.module';
+import { TelaUsuarioModule } from './tela-usuario/tela-usuario.modue';
+
+
+const routes: Routes = [
+  
+    {path: '', component: PaginaInicialComponent},
+    
+       
+  ];
+  
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),
+    PaginaInicialModule,
+    TelaUsuarioModule,
+          
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
